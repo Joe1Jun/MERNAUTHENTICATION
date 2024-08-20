5375d8c916df3f7244293ac32e5c95eab751105c
Joe1Jun <junker91joe@gmail.com>
2024-08-20 16:14:21 +0200

    Add Joi validation function for user registration
    - Implemented  function to validate user registration data using Joi.
    - Defined validation schema to ensure required fields (firstName, lastName, email, password) are present and correctly formatted.
    - Return validation result object containing  and  properties.
    - Integrated validation function into the registration route to handle user input validation before processing.

--- server/models/user.js
+++ server/models/user.js
@@ -21,3 +21,16 @@ userSchema.methods.generateAuthToken = function () {
 };
 
 const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+    const schema = Joi.object({
+        firstName: Joi.string().required().label("First Name"),
+        lastName: Joi.string().required().label("Last Name"),
+        email: Joi.string().required().label("Email"),
+        password: Joi.string().required().label("Password"),
+        
+    })
+    return schema.validate(data)
+}
+
+module.exports = {User, validate}

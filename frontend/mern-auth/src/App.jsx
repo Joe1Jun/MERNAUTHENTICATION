8919b54578497423bc2739639c0209f28dc421e1
Joe1Jun <junker91joe@gmail.com>
2024-08-22 16:30:55 +0200

    feat: Implement Main Component with Logout Functionality
    Created the Main component with a navigation bar displaying the application name
    Added a logout button that removes the authentication token from localStorage and reloads the page
    Styled the component using CSS modules for consistent and scoped styling

--- frontend/mern-auth/src/App.jsx
+++ frontend/mern-auth/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom'
+import Main from './Component/Main'
+import SignUp from './Component/SignUp/index';
+import Login from './Component/Login';
 import SignUp from './Component/SignUp/index';
 
 
 function App() {
   
+  const user = localStorage.getItem("token")
 
   return (
-    <SignUp/>
+
+    <Routes>
+      <Route path = '/' exact element= {<Main />} />
+
+       </Routes>
   );
 }
 
